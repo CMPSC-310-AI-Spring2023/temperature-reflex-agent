38235a59ae04da430c4644016cbcfffb9c08cc3a
Nic <ingersonmeacham01@allegheny.edu>
2023-02-01 15:36:01 -0500

    feat: Implement basic TMP36 functionality in Application.ino

--- src/lab01/Application/Application.ino
+++ src/lab01/Application/Application.ino
@@ -3,10 +3,20 @@ Andrew Briercheck
 Nic Ingerson
 */
 
+//Create instance of TMP36
+TMP36 myTMP36(E16, 3.3);
+//Set desired temperature of the environment
+float desiredTemp = 69.0;
+
 void setup() {
   TODO
 }
 
 void loop() {
-  TODO                      
+  //Get current temperature of the environment
+  float envTemp = myTMP36.getTempF();
+  //Check if the environment temperature deviates by 10 degrees
+  if (envTemp < desiredTemp - 10.0 || envTemp > desiredTemp + 10.0) {
+    return;
+  }
 }
