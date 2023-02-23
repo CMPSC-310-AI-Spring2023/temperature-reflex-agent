ac7152d91d6fde0e8a9f3e20a8648997fe4604a9
Nic <ingersonmeacham01@allegheny.edu>
2023-02-23 11:18:58 -0500

    docs: Convert report.md to README.md

--- src/lab01/Application/Application.ino
+++ src/lab01/Application/Application.ino
@@ -11,11 +11,13 @@ int led = 3;
 //Set desired temperature of the environment
 float desiredTemp = 20.00;
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop(){
+void loop()
+{
   sensorInput = analogRead(A0);
   temp = (double)sensorInput / 1024; //find percentage of input reading
   temp = temp * 5; //multiply by 5V to get voltage
@@ -23,16 +25,19 @@ void loop(){
   temp = temp * 100; //Convert to degrees (celsius)
 
   //turn led on if temperature deviates 5 degrees C from desired temp
-  if (temp < desiredTemp - 2.0 || temp > desiredTemp + 2.0){
+  if (temp < desiredTemp - 2.0 || temp > desiredTemp + 2.0)
+  {
     digitalWrite(led, HIGH);
     //debugging output
-    Serial.println("LED On");
+    Serial.print("LED On");
   }
-  else {
+  else
+  {
     digitalWrite(led, LOW); //turn off LED
   }
 
   //print the current temperature
   Serial.print("Current Temperature: ");
   Serial.println(temp);
+  delay(2000);
 }
